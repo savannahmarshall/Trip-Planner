cc91e00a0ef43cef53ef5fe26129d0071f8c5c55
savannahmarshall <165843690+savannahmarshall@users.noreply.github.com>
2024-07-18 20:53:40 -0700

    Delete public directory

--- public/js/login.js
+++
@@ -1,9 +0,0 @@
-// Get the modal
-const modal = document.getElementById('login-modal');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
